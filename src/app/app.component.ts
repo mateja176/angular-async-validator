fb2866acaba4f84770f9002b2cfb330228672cf2
Mateja Petrovic <mateja176@gmail.com>
2019-03-09 01:32:30 +0100

    Adds reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { map, tap } from 'rxjs/operators';
+import { checkEmailAvailability } from './is-email-taken/check-email-availability';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'async-validator';
+export class AppComponent implements OnInit {
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
+
+  ngOnInit() {
+    const emails$ = this.http
+      .get<any>('https://jsonplaceholder.typicode.com/users')
+      .pipe(
+        map(users => users.emails),
+        tap(console.log),
+      );
+
+    this.myForm = this.fb.group({
+      name: ['', Validators.required],
+      email: [
+        '',
+        [Validators.required, Validators.email],
+        checkEmailAvailability(emails$),
+      ],
+    });
+  }
 }
