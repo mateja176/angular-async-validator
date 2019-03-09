5be52f43519bd6e3072e24a1fd58019fbebcdb58
Mateja Petrovic <mateja176@gmail.com>
2019-03-09 12:23:50 +0100

    Adds errors$ reflacting form control errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import toPairs from 'ramda/es/toPairs';
 import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 import { emailAvailabilityValidator } from './email-availability.validator/email-availability.validator';
 import { emails$ } from './emails.service/emails.service';
 
@@ -12,9 +13,11 @@ import { emails$ } from './emails.service/emails.service';
 })
 export class AppComponent implements OnInit {
   myForm: FormGroup;
-  formValid$: Observable<boolean>;
 
-  constructor(private fb: FormBuilder, private http: HttpClient) {}
+  // errors$: Observable<ValidationErrors>; // ? does not accommodate for field names
+  errors$: Observable<any>;
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.myForm = this.fb.group({
@@ -25,5 +28,18 @@ export class AppComponent implements OnInit {
         emailAvailabilityValidator(emails$),
       ],
     });
+
+    this.errors$ = this.myForm.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      map(() =>
+        toPairs(this.myForm.controls).reduce(
+          (errors, [key, value]) => ({ ...errors, [key]: value.errors }),
+          {},
+        ),
+      ),
+    );
+
+    this.errors$.subscribe(console.log);
   }
 }
