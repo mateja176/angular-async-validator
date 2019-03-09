a29dafcfd93acdf612d9fa4bcd40d16175cac02a
Mateja Petrovic <mateja176@gmail.com>
2019-03-09 11:33:20 +0100

    Adds minLength requirement to name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.myForm = this.fb.group({
-      name: ['', Validators.required],
+      name: ['', [Validators.required, Validators.minLength(2)]],
       email: [
         '',
         [Validators.required, Validators.email],
