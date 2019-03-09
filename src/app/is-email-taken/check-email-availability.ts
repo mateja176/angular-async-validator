7b8abc1bc77e3b4b499338119b4a2be9492fc0cf
Mateja Petrovic <mateja176@gmail.com>
2019-03-09 01:27:42 +0100

    Adds debounceTIme and distinctUntilChanged

--- src/app/is-email-taken/check-email-availability.ts
+++ src/app/is-email-taken/check-email-availability.ts
@@ -1,7 +1,13 @@
 import { AbstractControl } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { isEmailTaken } from './is-email-taken';
 
 export const checkEmailAvailability = (emails$: Observable<string[]>) => (
   emailField: AbstractControl,
-) => emails$.pipe(isEmailTaken(emailField.value));
+) =>
+  emails$.pipe(
+    debounceTime(500),
+    distinctUntilChanged(),
+    isEmailTaken(emailField.value),
+  );
