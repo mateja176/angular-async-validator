0a314eee428f3aa12bd3317ddedd467c085cb44f
Mateja Petrovic <mateja176@gmail.com>
2019-03-09 10:55:51 +0100

    Adds users to emails operator

--- src/app/emails.service/emails.service.ts
+++ src/app/emails.service/emails.service.ts
@@ -1,9 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { of, pipe } from 'rxjs';
 import { delay, map, mergeMap, toArray } from 'rxjs/operators';
 
-export const emails$ = of([{ email: 'a@b.c' }]).pipe(delay(1000));
+export const usersToEmails = pipe(
+  mergeMap((users: { email: string }[]) => users),
+  map((user: { email: string }) => user.email),
+  toArray(),
+);
+
+export const emails$ = of([{ email: 'a@b.c' }]).pipe(
+  delay(1000),
+  usersToEmails,
+);
 
 @Injectable({
   providedIn: 'root',
@@ -13,9 +22,5 @@ export class EmailsService {
 
   emails$ = this.http
     .get<{ email: string }[]>('https://jsonplaceholder.typicode.com/users')
-    .pipe(
-      mergeMap(users => users),
-      map(user => user.email),
-      toArray(),
-    );
+    .pipe(usersToEmails);
 }
